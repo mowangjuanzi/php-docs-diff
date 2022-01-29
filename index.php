c702519f1b896c53ecefa8a1686dc92f339c630b
mowangjuanzi <baoguoxiao0538@hotmail.com>
2022-01-29 17:25:18 +0800

    更新功能，修复bug

--- index.php
+++ index.php
@@ -8,7 +8,20 @@ use Symfony\Component\Console\SingleCommandApplication;
 
 require __DIR__ . "/vendor/autoload.php";
 
-function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputInterface $output)
+const FlagTranslate = 1 << 0; // 翻译待更新的
+const FlagMissing = 1 << 1; // 未翻译的
+const FlagDelete = 1 << 2; // 待删除的
+
+/**
+ * 循环内容
+ * @param string $en_dir 英文目录
+ * @param string $zh_dir 中文目录
+ * @param string $parent_root 根目录
+ * @param OutputInterface $output Symfony/Console 的输出对象
+ * @param int $flags 设置输出格式 默认 FlagTranslate | FlagMissing | FlagDelete
+ * @return void
+ */
+function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputInterface $output, int $flags = 0)
 {
     $zh_file_map = [];
 
@@ -32,13 +45,9 @@ function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputI
 
     /** @var SplFileInfo $item */
     foreach ($en_iterator as $item) {
-        if ($item->isDir()) {
+        if ($item->isDir() && $item->getBasename()[0] != '.') { // 过滤隐藏文件
             $tmp_en_iterator['dir'][$item->getBasename()] = $item;
-        } else {
-            if (!in_array($item->getExtension(), ['xml', 'ent']) || $item->getFilename()[0] === '.' || preg_match("/entities.*.xml/", $item->getFilename()) == 1) {
-                continue;
-            }
-
+        } elseif ($item->isFile() && in_array($item->getExtension(), ['xml', 'ent']) && preg_match("/entities.*.xml/", $item->getFilename()) == 0) { // 过滤扩展名不正确或者文件名应该屏蔽掉的文件名
             $tmp_en_iterator['file'][$item->getBasename()] = $item;
         }
     }
@@ -48,7 +57,7 @@ function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputI
 
     // 循环目录
     foreach ($tmp_en_iterator['dir'] as $item) {
-        foreachDir($en_dir . "/" . $item->getFilename(), $zh_dir . "/" . $item->getFilename(), $parent_root, $output);
+        foreachDir($en_dir . "/" . $item->getFilename(), $zh_dir . "/" . $item->getFilename(), $parent_root, $output, $flags);
     }
 
     // 循环文件
@@ -61,42 +70,61 @@ function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputI
             $output->writeln("出错: " . "cd {$item->getPath()} && git log -1 --pretty=%H ./{$item->getFilename()} ");
         }
 
+        // version.xml 不用翻译
+        if ($item->getFilename() == 'versions.xml') {
+            if (isset($zh_file_map['versions.xml'])) {
+                $output->writeln("删除：" . substr($item, strlen($parent_root) + 1));
+            };
+
+            continue;
+        }
+
         if (!isset($zh_file_map[$item->getFilename()])) {
-            if ($item->getFilename() != 'versions.xml') {
+            if ($flags & FlagMissing) {
                 $output->writeln("缺失：" . substr($item->getPathname(), strlen($parent_root) + 1));
             }
             continue;
         }
 
-        $zh_file = file_get_contents($zh_file_map[$item->getFilename()]);
-        $zh_file = explode("\n", $zh_file);
+        if ($flags & FlagTranslate) {
+            $zh_file = file_get_contents($zh_file_map[$item->getFilename()]);
+            $zh_file = explode("\n", $zh_file);
 
-        $zh_commit_id = '';
-        foreach ($zh_file as $zh_file_item) {
-            $zh_file_item = trim($zh_file_item);
-            if (str_starts_with($zh_file_item, "<!-- EN-Revision:")) {
-                $zh_file_item = trim(substr($zh_file_item, 17));
-                $zh_commit_id = substr($zh_file_item, 0, strpos($zh_file_item, ' '));
-                break;
+            $zh_commit_id = '';
+            foreach ($zh_file as $zh_file_item) {
+                $zh_file_item = trim($zh_file_item);
+                if (str_starts_with($zh_file_item, "<!-- EN-Revision:")) {
+                    $zh_file_item = trim(substr($zh_file_item, 17));
+                    $zh_commit_id = substr($zh_file_item, 0, strpos($zh_file_item, ' '));
+                    break;
+                } elseif (str_starts_with($zh_file_item, "<!-- \$EN-Revision:")) {
+                    $zh_file_item = trim(substr($zh_file_item, 18));
+                    $zh_commit_id = substr($zh_file_item, 0, strpos($zh_file_item, ' '));
+                    break;
+                }
             }
-        }
 
-        if ($zh_commit_id != $en_commit_id) {
-            $output->writeln("翻译：" . substr($item->getPathname(), strlen($parent_root) + 1) . " " . $zh_commit_id);
+            if ($zh_commit_id != $en_commit_id) {
+                $output->writeln("翻译：" . substr($item->getPathname(), strlen($parent_root) + 1) . " " . $zh_commit_id);
+            }
         }
 
         unset($zh_file_map[$item->getFilename()]);
     }
 
-    foreach ($zh_file_map as $item) {
-        $output->writeln("删除：" . substr($item, strlen($parent_root) + 1));
+    if ($flags & FlagDelete) {
+        foreach ($zh_file_map as $item) {
+            $output->writeln("删除：" . substr($item, strlen($parent_root) + 1));
+        }
     }
 }
 
 (new SingleCommandApplication())
     ->setName("docs-diff")
-    ->setVersion("0.0.1")
+    ->setVersion("0.0.2")
     ->addOption("pull", 'p', InputOption::VALUE_NEGATABLE, "是否拉取最新 en 代码")
+    ->addOption("missing", 'm', InputOption::VALUE_NEGATABLE, "是否忽略未翻译的")
+    ->addOption("translate", 't', InputOption::VALUE_NEGATABLE, "是否忽略未翻译到最新的")
     ->addArgument("dir", InputArgument::IS_ARRAY, "目录名称，如果不指定则为全部", [])
     ->setCode(function (InputInterface $input, OutputInterface $output) {
         $parent_root = realpath(__DIR__ . "/../");
@@ -109,15 +137,22 @@ function foreachDir(string $en_dir, string $zh_dir, string $parent_root, OutputI
             passthru("git -C $en_doc_root pull");
         }
 
+        $flags = FlagDelete | FlagTranslate;
+
+        $flag_miss = $input->getOption('missing');
+        if ($flag_miss != true) {
+            $flags = $flags | FlagMissing;
+        }
+
         // 执行文件判断
         $dirs = $input->getArgument("dir");
         if ($dirs) {
             foreach ($dirs as $item) {
                 $item = ltrim($item, './');
-                foreachDir($en_doc_root . "/" . $item, $zh_doc_root . "/" . $item, $parent_root, $output);
+                foreachDir($en_doc_root . "/" . $item, $zh_doc_root . "/" . $item, $parent_root, $output,  $flags);
             }
         } else {
-            foreachDir($en_doc_root, $zh_doc_root, $parent_root, $output);
+            foreachDir($en_doc_root, $zh_doc_root, $parent_root, $output, $flags);
         }
 
     })->run();
